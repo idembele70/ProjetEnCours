fef5448cda8bd6e056f188714838eec4399ef050
idembele70 <idembele70@gmail.com>
2021-01-02 20:53:43 +0100

    ajout du portfolio responsive

---
+++ Portfolio_responsive/src/js/main.js
@@ -0,0 +1,19 @@
+//ouvrir Github
+
+function gitHub() {
+    window.open("https://github.com/idembele70/Projet_Terminer","_blank");
+}
+
+// slider Section MES TRAVAUX
+var slide = new Array("src/assets/images/todoimg1.png","src/assets/images/todoimg2.png","src/assets/images/todoimg3.png");
+var numero = 0;
+function changerSlide(sens) {
+  numero = numero + sens;
+  if (numero < 0) {
+    numero = slide.length - 1;
+  }
+  if (numero > slide.length -1) {
+    numero = 0;
+  }
+  document.getElementById("sliderMainImg").src = slide[numero];
+}
