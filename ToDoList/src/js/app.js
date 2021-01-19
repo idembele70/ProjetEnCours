1fe7c150fcbaea7543ceed919bfb3c83c035fe7c
idembele70 <idembele70@gmail.com>
2021-01-19 22:43:13 +0100

    Todo Add items style and add todo list

--- ToDoList/src/js/app.js
+++ ToDoList/src/js/app.js
@@ -5,7 +5,7 @@ const btnAddTodo = document.querySelector('.btnAddTodo');
 const ulList = document.getElementById('ulList');
 
 // Ecouteurs
-btnAddTodo.addEventListener('click',addTodo);
+btnAddTodo.addEventListener('click', addTodo);
 
 // Functions
 function JourDeLaSemaine() {
@@ -37,15 +37,39 @@ function JourDeLaSemaine() {
             "nothing";
             break;
     }
-spanJour.innerText += " " + day;
+    spanJour.innerText += " " + day;
 }
 JourDeLaSemaine();
 
-function addTodo(todoWrited) {
+function addTodo(inputValue) {
+    const todoDivContainer = document.createElement('div');
+    const divContainerRow = document.createElement('div');
+    divContainerRow.className = "row liRow";
+    todoDivContainer.className = "container";
 
-    
+    const li = document.createElement('li');
+    // const checkBox = document.createElement('input');
+    // checkBox.type = 'checkbox';
+    // checkBox.classList.add('checked','col-1');
+    const todo = document.createElement('input');
+    todo.disabled = true;
+    todo.classList.add('col-9', 'liInput');
+    let btnDelete = document.createElement('button');
+    btnDelete.classList.add('col-3', 'btnDelete');
+    btnDelete.innerHTML = '<i class="far fa-minus-square"></i>';
+
+    // divContainerRow.appendChild(checkBox);
+    divContainerRow.appendChild(todo);
+    divContainerRow.appendChild(btnDelete);
+    todoDivContainer.appendChild(divContainerRow);
+    li.appendChild(todoDivContainer);
+    ulList.appendChild(li);
 }
 
 function name(params) {
-    
+
+}
+
+function liste() {
+
 }
